3aa928027cdf7809e9c5929fe7d8c0e561bb8aa3
CoffeeHaus <neumann6994@gmail.com>
2019-12-17 09:07:15 -0600

    Update main.go

--- client/main.go
+++ client/main.go
@@ -5,5 +5,11 @@ import "fmt"
 // this is a comment
 
 func main() {
+    if runtime.GOOS == "windows" {
+    fmt.Println("Hello from Windows")
+        }
+    else if runtime.GOOS == 'linux' {    // also can be specified to FreeBSD
+    fmt.Println('Unix/Linux type OS detected')
+        }
     fmt.Println("Hello World")
 }
